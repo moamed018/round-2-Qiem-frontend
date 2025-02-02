3ea5faca7e9f1976b11de2cda0515b988e88f6cb
moamed018 <moamed018@gmail.com>
2025-02-02 22:57:40 +0200

    Revert "ctreat login page"
    This reverts commit 31b91d7a3e074d0c73193d7133c1412506739519.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router";
-import HomePage from "./main/pages/HomePage";
-
 import './App.css'
-import Login from "./main/pages/Login";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='text-6xl'>Real Estate App</div>
   )
 }
 
